5f8d159a67c114b8cd4fba1844940bb7e39e2fad
Anton Chuvashov <antonchuvashow@gmail.com>
2023-12-08 12:52:29 +0500

    add open question

--- questions/api/resources/answer.py
+++ questions/api/resources/answer.py
@@ -3,7 +3,7 @@ from sqlalchemy import select
 
 from api.utils import abort_if_doesnt_exist
 from models.db_session import create_session
-from models.questions import QuestionAnswer
+from models.questions import AnswerRecord
 
 # Request parser for filtering answer resources based on person_id and question_id
 fields_parser = reqparse.RequestParser()
@@ -13,42 +13,42 @@ fields_parser.add_argument('question_id', type=int, required=False)
 
 class AnswerResource(Resource):
     """
-    Resource for handling individual QuestionAnswer instances.
+    Resource for handling individual AnswerRecord instances.
     """
 
-    @abort_if_doesnt_exist("answer_id", QuestionAnswer)
+    @abort_if_doesnt_exist("answer_id", AnswerRecord)
     def get(self, answer_id):
         """
-        Get the details of a specific QuestionAnswer.
+        Get the details of a specific AnswerRecord.
 
         Args:
-            answer_id (int): The ID of the QuestionAnswer.
+            answer_id (int): The ID of the AnswerRecord.
 
         Returns:
-            tuple: A tuple containing the details of the QuestionAnswer and HTTP status code.
+            tuple: A tuple containing the details of the AnswerRecord and HTTP status code.
         """
         with create_session() as db:
-            # Retrieve the QuestionAnswer from the database and convert it to a dictionary
-            db_answer = db.get(QuestionAnswer, answer_id).to_dict(rules=("-question",))
+            # Retrieve the AnswerRecord from the database and convert it to a dictionary
+            db_answer = db.get(AnswerRecord, answer_id).to_dict(rules=("-question",))
         return db_answer, 200
 
 
 class AnswerListResource(Resource):
     """
-    Resource for handling lists of QuestionAnswer instances.
+    Resource for handling lists of AnswerRecord instances.
     """
 
     def get(self):
         """
-        Get a list of QuestionAnswer instances based on optional filtering parameters.
+        Get a list of AnswerRecord instances based on optional filtering parameters.
 
         Returns:
-            tuple: A tuple containing the list of QuestionAnswer instances and HTTP status code.
+            tuple: A tuple containing the list of AnswerRecord instances and HTTP status code.
         """
         # Parse the filtering parameters from the request
         args = {k: v for k, v in fields_parser.parse_args().items() if v is not None}
         with create_session() as db:
-            # Retrieve QuestionAnswer instances from the database based on the filtering parameters
-            answers = [a.to_dict(rules=("-question",)) for a in db.scalars(select(QuestionAnswer).filter_by(**args))]
+            # Retrieve AnswerRecord instances from the database based on the filtering parameters
+            answers = [a.to_dict(rules=("-question",)) for a in db.scalars(select(AnswerRecord).filter_by(**args))]
 
         return answers, 200
--- questions/models/questions.py
+++ questions/models/questions.py
@@ -11,7 +11,7 @@ from .db_session import SqlAlchemyBase
 
 class AnswerState(enum.Enum):
     """
-    Enumeration representing the state of a QuestionAnswer.
+    Enumeration representing the state of AnswerRecord.
 
     Attributes:
         NOT_ANSWERED (int): The answer has not been provided.
@@ -21,6 +21,18 @@ class AnswerState(enum.Enum):
     NOT_ANSWERED = 0
     TRANSFERRED = 1
     ANSWERED = 2
+
+
+class QuestionType(enum.Enum):
+    """
+    Enumeration representing the type of AnswerRecord.
+
+    Attributes:
+        TEST (int): The question suggests answer in a test form.
+        OPEN (int): The question suggest answer in a text form.
+    """
+    TEST = 0
+    OPEN = 1
 
 
 class QuestionGroupAssociation(SqlAlchemyBase, SerializerMixin):
@@ -48,10 +60,12 @@ class Question(SqlAlchemyBase, SerializerMixin):
         text (str): The text of the question.
         subject (Optional[str]): The subject of the question (optional).
         options (str): JSON-encoded options for the question.
-        answer (int): The correct answer to the question.
+        answer (str): The correct answer to the question.
         groups (List[QuestionGroupAssociation]): The groups associated with the question.
         level (int): The difficulty level of the question.
         article_url (Optional[str]): URL to an article related to the question (optional).
+        type (QuestionType): The type of the answer (TEST, OPEN)
+
     """
     __tablename__ = "questions"
 
@@ -59,13 +73,14 @@ class Question(SqlAlchemyBase, SerializerMixin):
     text: Mapped[str]
     subject: Mapped[Optional[str]]
     options: Mapped[str]
-    answer: Mapped[int]
+    answer: Mapped[str]
     groups: Mapped[List[QuestionGroupAssociation]] = relationship()
     level: Mapped[int]
     article_url: Mapped[Optional[str]]
+    type: Mapped[QuestionType] = mapped_column(default=QuestionType.TEST)
 
 
-class QuestionAnswer(SqlAlchemyBase, SerializerMixin):
+class AnswerRecord(SqlAlchemyBase, SerializerMixin):
     """
     Represents an answer to a question.
 
@@ -74,13 +89,14 @@ class QuestionAnswer(SqlAlchemyBase, SerializerMixin):
         question_id (int): Foreign key referencing the questions table.
         question (Question): Relationship to the corresponding question.
         person_id (str): The ID of the person providing the answer.
-        person_answer (Optional[int]): The answer provided by the person (optional).
+        person_answer (Optional[str]): The answer provided by the person (optional).
         answer_time (Optional[datetime.datetime]): The time when the answer was provided (optional).
         ask_time (datetime.datetime): The time when the question was asked.
         state (AnswerState): The state of the answer (NOT_ANSWERED, TRANSFERRED, ANSWERED).
+        points (int): Amount of points scored for this answer (from 0 to 1)
 
     Methods:
-        __repr__: Returns a string representation of the QuestionAnswer.
+        __repr__: Returns a string representation of the AnswerRecord.
     """
     __tablename__ = 'answers'
 
@@ -88,10 +104,24 @@ class QuestionAnswer(SqlAlchemyBase, SerializerMixin):
     question_id: Mapped[int] = mapped_column(ForeignKey("questions.id"))
     question: Mapped["Question"] = relationship(lazy="joined")
     person_id: Mapped[str] = mapped_column()
-    person_answer: Mapped[Optional[int]]
+    person_answer: Mapped[Optional[str]]
     answer_time: Mapped[Optional[datetime.datetime]]
     ask_time: Mapped[datetime.datetime] = mapped_column()
     state: Mapped[AnswerState]
+    points: Mapped[float] = mapped_column(default=0)
 
     def __repr__(self):
-        return f"<QuestionAnswer(text={self.question_id}, state={self.state}, person_id={self.person_id})>"
+        return f"<AnswerRecord(text={self.question_id}, state={self.state}, person_id={self.person_id})>"
+
+    def calculate_points(self):
+        match self.question.type:
+            case QuestionType.TEST:
+                if self.question.answer == self.person_answer:
+                    self.points = 1
+                else:
+                    self.points = 0
+
+            case QuestionType.OPEN:
+                # TODO: add adequate calculation of points
+
+                self.points = 0
