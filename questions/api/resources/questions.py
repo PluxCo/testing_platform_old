af3e55fa6f00b5b6554c225cdf92a37ed59c0d32
enard <serg.miass340@gmail.com>
2024-01-11 20:11:19 +0500

    added view_parser to QuestionsResource

--- questions/api/resources/questions.py
+++ questions/api/resources/questions.py
@@ -2,9 +2,9 @@ import json
 import logging
 
 from flask_restful import Resource, reqparse
-from sqlalchemy import select, update, delete, or_, desc, asc
+from sqlalchemy import select, update, delete, or_, desc, asc, func
 
-from api.utils import abort_if_doesnt_exist
+from api.utils import abort_if_doesnt_exist, view_parser
 from models.db_session import create_session
 from models.questions import Question, QuestionGroupAssociation, QuestionType, AnswerRecord
 
@@ -30,10 +30,8 @@ create_data_parser.add_argument('level', type=int, required=True)
 create_data_parser.add_argument('article_url', type=str, required=False)
 create_data_parser.add_argument('type', type=QuestionType, required=False)
 
-sorted_question_data_parser = reqparse.RequestParser()
-sorted_question_data_parser.add_argument('search_string', type=str, required=False)
-sorted_question_data_parser.add_argument('column_to_order', type=str, required=False)
-sorted_question_data_parser.add_argument('descending', type=bool, required=False)
+sorted_question_data_parser = view_parser.copy()
+sorted_question_data_parser.add_argument('search_string', type=str, required=False, location="args", default="")
 
 
 class QuestionResource(Resource):
@@ -60,7 +58,7 @@ class QuestionResource(Resource):
         return db_question, 200
 
     @abort_if_doesnt_exist("question_id", Question)
-    def post(self, question_id):
+    def patch(self, question_id):
         # TODO: fix update of questions so that after changing options the answers stay correct.
         """
         Update the details of a specific Question.
@@ -128,32 +126,31 @@ class QuestionsListResource(Resource):
         args = sorted_question_data_parser.parse_args()
 
         search_string = args['search_string']
-        column_to_order = args['column_to_order']
-        descending = args['descending']
-
-        if descending:
-            column_to_order = desc(column_to_order)
 
         with create_session() as db:
-            if search_string != "" or column_to_order != "":
-                questions = db.scalars(select(Question).
-                                       where(or_(Question.text.ilike(f"%{search_string}%"),
-                                                 Question.subject.ilike(f"%{search_string}%"),
-                                                 Question.options.ilike(f"%{search_string}%"),
-                                                 Question.level.ilike(f"%{search_string}%"),
-                                                 Question.article_url.ilike(f"%{search_string}%"))).
-                                       order_by(column_to_order))
-                db_question = [q.to_dict(rules=("-groups.id", "-groups.question_id")) for q in
-                               questions]
-            else:
-                # Retrieve Question instances from the database and convert them to dictionaries
-                db_question = [q.to_dict(rules=("-groups.id", "-groups.question_id")) for q in
-                               db.scalars(select(Question))]
-            for q in db_question:
+            total = db.scalar(select(func.count(Question.id)))
+
+            db_req = (select(Question, func.count(Question.id).over())
+                      .where(or_(Question.text.ilike(f"%{search_string}%"),
+                                 Question.subject.ilike(f"%{search_string}%"),
+                                 Question.options.ilike(f"%{search_string}%"),
+                                 Question.level.ilike(f"%{search_string}%"),
+                                 Question.article_url.ilike(f"%{search_string}%"))))
+
+            db_req = (db_req.order_by(args["orderBy"] if args["order"] == "asc" else desc(args["orderBy"]))
+                      .limit(args["resultsCount"])
+                      .offset(args["offset"]))
+
+            questions = []
+            results_filtered = 0
+            for a, results_filtered in db.execute(db_req):
+                questions.append(a.to_dict(rules=("-groups.id", "-groups.question_id")))
+
+            for q in questions:
                 if q["options"]:
                     q["options"] = json.loads(q["options"])
 
-        return db_question, 200
+        return {"results_total": total, "results_count": results_filtered, "questions": questions}, 200
 
     def post(self, **kwargs):
         """
