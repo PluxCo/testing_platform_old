6ddc6d277d4611bb72ad788407066de77600a4bd
enard <serg.miass340@gmail.com>
2024-01-11 21:08:35 +0500

    removed extra accessors

--- questions/data_accessors/auth_accessor.py
+++ questions/data_accessors/auth_accessor.py
@@ -3,62 +3,6 @@ import os
 import requests
 
 
-class Group:
-    def __init__(self, g_id: str,
-                 label: str):
-        self.id = g_id
-        self.label = label
-
-
-class GroupsDAO:
-    __host = os.getenv("FUSIONAUTH_DOMAIN")
-    __token = os.getenv("FUSIONAUTH_TOKEN")
-
-    @staticmethod
-    def set_host(resource: str, token: str):
-        GroupsDAO.__host = resource
-        GroupsDAO.__token = token
-
-    @staticmethod
-    def _construct(resp):
-        q = Group(resp["id"], resp["name"])
-        return q
-
-    @staticmethod
-    def get_group(group_id: str) -> Group:
-        resp = requests.get(f"{GroupsDAO.__host}/api/group/" + str(group_id),
-                            headers={"Authorization": GroupsDAO.__token})
-
-        if resp.status_code != 200:
-            raise Exception(resp.status_code, resp.text)
-        resp = resp.json()
-
-        return GroupsDAO._construct(resp["group"])
-
-    @staticmethod
-    def get_all_groups():
-        resp = requests.get(f"{GroupsDAO.__host}/api/group/search",
-                            headers={"Authorization": GroupsDAO.__token})
-        if resp.status_code != 200:
-            raise Exception(resp.status_code, resp.text)
-        resp = resp.json()
-
-        for g in resp["groups"]:
-            yield GroupsDAO._construct(g)
-
-    @staticmethod
-    def create_group(group: Group):
-        req = {
-            "group": {
-                "name": group.label
-            }
-        }
-
-        resp = requests.post(f"{GroupsDAO.__host}/api/group/",
-                             json=req,
-                             headers={"Authorization": GroupsDAO.__token})
-
-
 class Person:
     """
     Class representing a person/user with associated groups.
